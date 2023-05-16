60502f18c995ca5484b6fa817921ebecebd92298
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2023-05-16 17:38:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+prediction_1="";
+prediction_2="";
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">'
+
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/mIMOu8Rw5/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1="The first prediction is"+ prediction_1;
+    speak_data_2="The second prediction is"+ prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
+function predict(){
+    img = document.getElementById("captured_image");
+    Classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+
+
+
+        if (results[0].label=="thumbs_up"){
+            document.getElementById("result_emoji").innerHTML="&#128077";
+        }
+        if (results[0].label=="victory"){
+            document.getElementById("result_emoji").innerHTML="&#9996";
+        }
+        if (results[0].label=="amazing"){
+            document.getElementById("result_emoji").innerHTML="&#128076";
+        }
+        if (results[1].label=="thumbs_up"){
+            document.getElementById("result_emoji2").innerHTML="&#128077";
+        }
+        if (results[1].label=="vixtory"){
+            document.getElementById("result_emoji2").innerHTML="&#9996";
+        }
+        if (results[1].label=="amazing"){
+            document.getElementById("result_emoji2").innerHTML="&#128076";
+        }
+}
+}
